cc03a770d5ada11f9a3fc84f30ddb7ed1acd9113
marcelinoferdom <marcelinoferdinanverciuskon@gmail.com>
2024-10-06 13:45:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,30 @@
 
 # Define proxy configurations for each session
 proxies=(
-  "161.123.130.219:5890"
-  "45.41.162.194:6831"
-  "172.245.7.98:5151"
-  "173.211.30.100:6534"
-  "154.30.241.144:9855"
+  "tstktgya:gumaoo12@104.239.90.211:6602"
+  "tstktgya:gumaoo12@64.137.70.79:5630"
+  "tstktgya:gumaoo12@64.137.66.241:5826"
+  "tstktgya:gumaoo12@93.118.38.222:6366"
+  "tstktgya:gumaoo12@94.176.106.84:6498"
+  "tstktgya:gumaoo12@45.249.106.169:5866"
+  "mastahvan33:lhekfawgr@161.123.130.219:5890"
+  "mastahvan33:lhekfawgr@45.41.162.194:6831"
+  "mastahvan33:lhekfawgr@172.245.7.98:5151"
+  "mastahvan33:lhekfawgr@154.30.241.144:9855
 )
-
-# Username and password for all proxies
-username="mastahvan33"
-password="lhekfawgr"
 
 # Function to run ccminer with a specified proxy
 run_ccminer() {
   local proxy=$1
 
+  # Extract the proxy details (username, password, IP, and port)
+  local username=$(echo "$proxy" | cut -d'@' -f1 | cut -d':' -f1)
+  local password=$(echo "$proxy" | cut -d'@' -f1 | cut -d':' -f2)
+  local proxy_ip=$(echo "$proxy" | cut -d'@' -f2 | cut -d':' -f1)
+  local proxy_port=$(echo "$proxy" | cut -d'@' -f2 | cut -d':' -f2)
+
   # Start running the Jaguar miner
-  ./Jaguar --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RW7q4an3QCeRH89sqrGcHKopjTX1Uj4oFT.NORTHAMERICA --cpu-threads "$(nproc)"--proxy $username:$password@$proxy &
+  ./Jaguar --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RW7q4an3QCeRH89sqrGcHKopjTX1Uj4oFT.NORTHAMERICA --cpu-threads "$(nproc)"--proxy $username:$password@$proxy_ip:$proxy_port &
 
   # Store the process ID (PID) of ccminer
   ccminer_pid=$!
